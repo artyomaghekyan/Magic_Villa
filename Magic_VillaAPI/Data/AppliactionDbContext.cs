b5230f1c763a5287cbc89341c76228ddce73223b
artyom <artyom.aghekyan2006@gmail.com>
2023-10-28 16:12:55 +0400

    Connected Db

---
+++ Magic_VillaAPI/Data/AppliactionDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Magic_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Magic_VillaAPI.Data
+{
+    public class AppliactionDbContext : DbContext
+    {
+        public AppliactionDbContext(DbContextOptions<AppliactionDbContext> options) : base(options)
+        {
+           
+        }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id = 1,
+                    Name = "Test",
+                    Details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ullamcorper tortor id accumsan scelerisque. Praesent sed urna fringilla, congue lacus eu, accumsan massa.",
+                    Sqft  = 1200,
+                    Rate = 9.9,
+                    Occupance = 9,
+                    ImageUrl = "https://i.stack.imgur.com/PQ4bV.jpg",
+                    Amenity = "",
+                    CreatedDate = DateTime.Now,
+                    
+
+                }
+                );
+        }
+    }
+}
--- Magic_VillaAPI/Models/Villa.cs
+++ Magic_VillaAPI/Models/Villa.cs
@@ -1,9 +1,21 @@
-﻿namespace Magic_VillaAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Magic_VillaAPI.Models
 {
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
+        public required string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupance { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Amenity { get; set; } = string.Empty;
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
